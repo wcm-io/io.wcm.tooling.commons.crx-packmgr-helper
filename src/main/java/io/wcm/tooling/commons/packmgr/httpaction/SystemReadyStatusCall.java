61f00a75ccb4d751b1a209fb6e8418628561850a
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-09-15 13:28:03 +0200

    system ready status

---
+++ src/main/java/io/wcm/tooling/commons/packmgr/httpaction/SystemReadyStatusCall.java
@@ -0,0 +1,89 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2025 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.tooling.commons.packmgr.httpaction;
+
+import java.io.IOException;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
+import io.wcm.tooling.commons.packmgr.PackageManagerHttpActionException;
+
+/**
+ * Get bundle status from web console.
+ */
+public final class SystemReadyStatusCall implements HttpCall<SystemReadyStatus> {
+
+  private final CloseableHttpClient httpClient;
+  private final HttpClientContext context;
+  private final String systemReadyURL;
+
+  private static final Logger log = LoggerFactory.getLogger(SystemReadyStatusCall.class);
+  private static final JsonMapper JSON_MAPPER = JsonMapper.builder().build();
+
+  /**
+   * @param httpClient HTTP client
+   * @param context HTTP client context
+   * @param systemReadyURL System ready URL
+   */
+  public SystemReadyStatusCall(CloseableHttpClient httpClient, HttpClientContext context, String systemReadyURL) {
+    this.httpClient = httpClient;
+    this.context = context;
+    this.systemReadyURL = systemReadyURL;
+  }
+
+  @Override
+  public SystemReadyStatus execute() {
+    log.debug("Call URL: {}", systemReadyURL);
+
+    HttpGet method = new HttpGet(systemReadyURL);
+    try (CloseableHttpResponse response = httpClient.execute(method, context)) {
+
+      String responseString = EntityUtils.toString(response.getEntity());
+      if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+        throw PackageManagerHttpActionException.forHttpError(systemReadyURL, response.getStatusLine(), responseString);
+      }
+
+      return toSystemReadyStatus(responseString);
+    }
+    catch (IOException ex) {
+      throw PackageManagerHttpActionException.forIOException(systemReadyURL, ex);
+    }
+  }
+
+  static SystemReadyStatus toSystemReadyStatus(String jsonString) {
+    try {
+      return JSON_MAPPER.readValue(jsonString, SystemReadyStatus.class);
+    }
+    catch (JsonProcessingException ex) {
+      throw new PackageManagerHttpActionException("JSON parse failure: " + ex.getMessage(), ex);
+    }
+  }
+
+}
---
+++ src/test/java/io/wcm/tooling/commons/packmgr/httpaction/SystemReadyStatusCallTest.java
@@ -0,0 +1,76 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2025 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.tooling.commons.packmgr.httpaction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+
+import io.wcm.tooling.commons.packmgr.PackageManagerHttpActionException;
+
+class SystemReadyStatusCallTest {
+
+  @Test
+  void testToSystemReadyStatus() throws IOException {
+    // Load test JSON file
+    try (InputStream is = getClass().getResourceAsStream("/packmgr/systemreadyresponse.json")) {
+      String jsonContent = IOUtils.toString(is, StandardCharsets.UTF_8);
+
+      // Parse JSON using the static method
+      SystemReadyStatus status = SystemReadyStatusCall.toSystemReadyStatus(jsonContent);
+
+      // Verify overall result
+      assertNotNull(status);
+      assertEquals("CRITICAL", status.getOverallResult());
+
+      // Verify results list (do not check further details here)
+      assertNotNull(status.getResults());
+      assertEquals(7, status.getResults().size());
+    }
+  }
+
+  @Test
+  void testToSystemReadyStatus_InvalidJson() {
+    String invalidJson = "{ invalid json }";
+
+    assertThrows(
+        PackageManagerHttpActionException.class,
+        () -> SystemReadyStatusCall.toSystemReadyStatus(invalidJson)
+    );
+  }
+
+  @Test
+  void testToSystemReadyStatus_EmptyJson() {
+    String emptyJson = "{}";
+
+    // Should not throw exception, but fields should be null
+    SystemReadyStatus status = SystemReadyStatusCall.toSystemReadyStatus(emptyJson);
+    assertNotNull(status);
+    // Fields will be null since they're not present in JSON
+  }
+
+}
